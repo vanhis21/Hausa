b37d773e9561dda924319ddf6d1cb42fde63540e
vanhis21 <vanhis1738@gmail.com>
2020-07-19 19:47:50 +0300

    updated

--- js/styl.js
+++ js/styl.js
@@ -32,29 +32,3 @@ const maleNames = [
     return [gender, day, month, year];
   }
 
-// calculates week day
-function calculateWeekDay(birthDate) {
-  let [dayOfMonth, monthOfYear, yearOfBirth] = birthDate;
-  let zeroBasedCentury, yearOfCentury;
-  if (monthOfYear <= 2) {
-    monthOfYear += 12;
-    yearOfBirth -= 1;
-    }  
-
-// Split year to centuryCode & yearCode
-zeroBasedCentury = parseInt(yearOfBirth / 100);
-yearOfCentury = yearOfBirth % 100;
-let dayOfWeek =
-(dayOfMonth +
-parseInt(
-2.6 * (monthOfYear + 1) +
-yearOfCentury +
-parseInt(yearOfCentury / 4) +
-parseInt(zeroBasedCentury / 4) +
-5 * zeroBasedCentury
-)) %
-7;
-// return dayOfWeek as a zero-based index
-// dayOfWeek = (0 = Saturday, 1 = Sunday, 2 = Monday, ..., 6 = Friday)
-return dayOfWeek;
-}
